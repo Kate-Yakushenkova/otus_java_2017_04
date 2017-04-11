3f9d1d169a52836d677a9493a35a3872b2cd5b81
Якушенкова Катя <Kate-i-y@yandex.ru>
2017-04-11 18:45:56 +0300

    Незначительные изменения

---
+++ src/main/java/ru/otus/homework/homework1/Test.java
@@ -0,0 +1,9 @@
+package ru.otus.homework.homework1;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
--- src/main/java/ru/otus/java_homework/Test.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.java_homework;
-
-public class Test {
-
-    public static void main(String[] args) {
-
-    }
-
-}
