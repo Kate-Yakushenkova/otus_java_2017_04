d19d44fa23fc141b2869cb41b2162802a0e1235a
Катя <Kate-i-y@yandex.ru>
2017-06-23 01:56:05 +0300

    Разделение на модули

---
+++ homework11/src/main/java/ru/otus/homework/db/DBService.java
@@ -0,0 +1,57 @@
+package ru.otus.homework.db;
+
+import ru.otus.homework.cache.CacheEngineImpl;
+import ru.otus.homework.cache.MyElement;
+import ru.otus.homework.dataSets.User;
+import ru.otus.homework.exception.DBException;
+import ru.otus.homework.executors.ExecutorImpl;
+
+import java.lang.ref.SoftReference;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBService {
+
+    private ExecutorImpl executor;
+
+    public CacheEngineImpl<Long, User> cacheEngine = new CacheEngineImpl<>(5);
+
+    public DBService(Connection connection) {
+        this.executor = new ExecutorImpl(connection);
+    }
+
+    public long addUser(User user) throws DBException {
+        try {
+            executor.createTable(User.class);
+            executor.save(user);
+            return executor.getId(user);
+        } catch (SQLException e) {
+            throw new DBException(e);
+        }
+    }
+
+    public User getUser(long id) throws DBException {
+        try {
+            MyElement<Long, User> element = cacheEngine.get(id);
+            User user;
+            if (element != null) {
+                user = element.getValue().get();
+            } else {
+                user = executor.load(id, User.class);
+                cacheEngine.put(new MyElement<>(id, new SoftReference<>(user)));
+            }
+            return user;
+        } catch (SQLException e) {
+            throw new DBException(e);
+        }
+    }
+
+    public void clear() throws DBException {
+        try {
+            executor.clear(User.class);
+            cacheEngine.dispose();
+        } catch (SQLException e) {
+            throw new DBException(e);
+        }
+    }
+}
--- homework11/src/main/java/ru/otus/homework/main/Main.java
+++ homework11/src/main/java/ru/otus/homework/main/Main.java
@@ -4,14 +4,12 @@ import ru.otus.homework.cache.CacheEngineImpl;
 import ru.otus.homework.cache.MyElement;
 import ru.otus.homework.dataSets.User;
 import ru.otus.homework.db.DBConnection;
-import ru.otus.homework.db.UserDAO;
+import ru.otus.homework.db.DBService;
 import ru.otus.homework.exception.DBException;
-
-import java.lang.ref.SoftReference;
 
 public class Main {
 
-    private static final int NUMBER_USER = 10000;
+    private static final int NUMBER_USER = 10;
 
     /**
      * mysql> CREATE USER 'kate'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
@@ -19,53 +17,54 @@ public class Main {
      * mysql> GRANT ALL PRIVILEGES ON db_kate.* TO 'kate'@'localhost';
      * mysql> FLUSH PRIVILEGES;
      */
-    
+
     public static void main(String[] args) {
-        UserDAO userDAO = createUserDAO();
-        if (userDAO != null) {
-            CacheEngineImpl<Integer, User> cacheEngine = new CacheEngineImpl<>(5);
-
-            createUsers(NUMBER_USER, userDAO);
-
-            for (int i = 1; i <= NUMBER_USER; i++) {
-                User user = getUser(userDAO, (long)i);
-                cacheEngine.put(new MyElement<>(i, new SoftReference<>(user)));
-            }
+        DBService dbService = createUserDAO();
+        if (dbService != null) {
+            createUsers(NUMBER_USER, dbService);
+            getUsers(NUMBER_USER, dbService);
+            System.out.println("Hits: " + dbService.cacheEngine.getHitCount());
+            System.out.println("Miss: " + dbService.cacheEngine.getMissCount());
             System.out.println();
 
-            printCache(cacheEngine);
+            printCache(dbService.cacheEngine);
+            System.out.println("Hits: " + dbService.cacheEngine.getHitCount());
+            System.out.println("Miss: " + dbService.cacheEngine.getMissCount());
 
-            System.out.println("Hits: " + cacheEngine.getHitCount());
-            System.out.println("Miss: " + cacheEngine.getMissCount());
-
-            clear(userDAO);
-            cacheEngine.dispose();
+            clear(dbService);
         }
     }
 
-    private static UserDAO createUserDAO() {
+    private static void getUsers(int number, DBService dbService) {
+        for (int i = 1; i <= number; i++) {
+            User user = getUser(dbService, (long) i);
+        }
+        System.out.println();
+    }
+
+    private static DBService createUserDAO() {
         try {
             DBConnection dbConnection = new DBConnection();
             dbConnection.printConnectInfo();
             System.out.println();
-            return new UserDAO(dbConnection.getConnection());
+            return new DBService(dbConnection.getConnection());
         } catch (DBException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private static void createUsers(int number, UserDAO userDAO) {
+    private static void createUsers(int number, DBService dbService) {
         for (int i = 1; i <= number; i++) {
             User user = new User("Tom" + i, i);
-            insertUser(user, userDAO);
+            insertUser(user, dbService);
         }
         System.out.println();
     }
 
-    private static Long insertUser(User user, UserDAO userDAO) {
+    private static Long insertUser(User user, DBService dbService) {
         try {
-            long id = userDAO.addUser(user);
+            long id = dbService.addUser(user);
             System.out.println("User added with id: " + id);
             return id;
         } catch (DBException e) {
@@ -74,13 +73,13 @@ public class Main {
         return null;
     }
 
-    private static User getUser(UserDAO userDAO, Long id) {
+    private static User getUser(DBService dbService, Long id) {
         try {
             if (id == null) {
                 System.out.println("Пользователя с таким id не существует");
                 return null;
             }
-            User savedUser = userDAO.getUser(id);
+            User savedUser = dbService.getUser(id);
             System.out.println(id + ": " + savedUser);
             return savedUser;
         } catch (DBException e) {
@@ -89,17 +88,17 @@ public class Main {
         return null;
     }
 
-    private static void printCache(CacheEngineImpl<Integer, User> cacheEngine) {
-        for (int i = 1; i <= NUMBER_USER; i++) {
+    private static void printCache(CacheEngineImpl<Long, User> cacheEngine) {
+        for (long i = 1; i <= NUMBER_USER; i++) {
             MyElement element = cacheEngine.get(i);
             System.out.println("User " + i + " in cache: " + (element == null ? null : element.getValue().get()));
         }
         System.out.println();
     }
 
-    private static void clear(UserDAO userDAO) {
+    private static void clear(DBService dbService) {
         try {
-            userDAO.clear();
+            dbService.clear();
         } catch (DBException e) {
             e.printStackTrace();
         }
